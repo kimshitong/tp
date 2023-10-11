a2b7eff7dc456e5e1cc7a85bb1810b165d157f3c
kimshitong <shitongkim28@gmail.com>
2023-10-12 01:25:32 +0800

    Create Doctor Message

---
+++ src/main/java/seedu/address/logic/DoctorMessages.java
@@ -0,0 +1,45 @@
+package seedu.address.logic;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import seedu.address.logic.parser.Prefix;
+import seedu.address.model.person.doctor.Doctor;
+
+/**
+ * Container for user visible messages.
+ */
+public class DoctorMessages {
+
+    public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The doctor index provided is invalid";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d doctors listed!";
+    public static final String MESSAGE_DUPLICATE_FIELDS =
+                "Multiple values specified for the following single-valued field(s): ";
+
+    /**
+     * Returns an error message indicating the duplicate prefixes.
+     */
+    public static String getErrorMessageForDuplicatePrefixes(Prefix... duplicatePrefixes) {
+        assert duplicatePrefixes.length > 0;
+
+        Set<String> duplicateFields =
+                Stream.of(duplicatePrefixes).map(Prefix::toString).collect(Collectors.toSet());
+
+        return MESSAGE_DUPLICATE_FIELDS + String.join(" ", duplicateFields);
+    }
+
+    /**
+     * Formats the {@code doctor} for display to the user.
+     */
+    public static String format(Doctor doctor) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(doctor.getName())
+                .append("; NRIC: ")
+                .append(doctor.getNric());
+        return builder.toString();
+    }
+
+}
